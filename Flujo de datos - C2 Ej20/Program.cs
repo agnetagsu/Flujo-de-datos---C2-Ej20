2f7d3f914cf07a03e7f317dd4b7f3b0b70d3b333
Agneta <agnetagsu@yahoo.es>
2021-05-22 21:38:14 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ventas;
+            int suma = 0;
+
+            Console.WriteLine("Escribe el numero de ventas:");
+            ventas = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= ventas; i++)
+            {
+                Console.WriteLine("Introduzca la venta "+ i);
+                suma = suma + Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma total de las ventas es: "+ suma);
+        }
+    }
+}
